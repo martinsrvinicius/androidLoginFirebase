a232d032d7303df2839322294a65eecbc9b330d6
Vinicius Martins <martinsrvinicius@gmail.com>
2022-12-29 12:13:39 -0300

    Sign in Exception

--- app/src/main/java/com/martinsrvinicius/projetofirebase/FormLogin.java
+++ app/src/main/java/com/martinsrvinicius/projetofirebase/FormLogin.java
@@ -1,18 +1,25 @@
 package com.martinsrvinicius.projetofirebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.martinsrvinicius.projetofirebase.databinding.ActivityFormLoginBinding;
 
 public class FormLogin extends AppCompatActivity {
 
     private ActivityFormLoginBinding binding;
-    private boolean isProgress = false;
+    private String[] messages = {"Preencha todos os campos"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,9 +40,54 @@ public class FormLogin extends AppCompatActivity {
         binding.btnEnter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                String email = binding.editEmail.getText().toString();
+                String password = binding.editPassword.getText().toString();
+                if (email.isEmpty() || password.isEmpty()) {
+                    Snackbar snackbar = Snackbar.make(view, messages[0], Snackbar.LENGTH_SHORT);
+                    snackbar.setBackgroundTint(Color.WHITE);
+                    snackbar.setTextColor(Color.BLACK);
+                    snackbar.show();
+                } else {
+                    authUser(view, email, password);
+                }
             }
         });
     }
 
+    private void authUser(View view, String email, String password) {
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            binding.progressBar.setVisibility(View.VISIBLE);
+
+                            new Handler().postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    profileIntent();
+                                }
+                            }, 3000);
+                        } else {
+                            String erro;
+                            try {
+                                throw task.getException();
+                            } catch (Exception e) {
+                                erro = "Erro ao logar usuário";
+                            }
+                            Snackbar snackbar = Snackbar.make(view, erro, Snackbar.LENGTH_SHORT);
+                            snackbar.setBackgroundTint(Color.WHITE);
+                            snackbar.setTextColor(Color.BLACK);
+                            snackbar.show();
+                        }
+                    }
+                });
+    }
+
+    private void profileIntent() {
+        Intent intent = new Intent(FormLogin.this, Profile.class);
+        startActivity(intent);
+        finish();
+    }
 
 }
